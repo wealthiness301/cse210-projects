8f5f1e01b16c14a99268803e57683d1471e09c7c
wealthiness301 <osaigbovowealth3@gmail.com>
2025-09-10 19:29:25 +0100

    commit4

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        Resume resumes = new Resume();
+        resumes._name = "Wealth Osaigbovo";
+        resumes._job.Add(job1);
+        resumes._job.Add(job2);
+        resumes.Display();
     }
 }
---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,15 @@
+using System;
+public class Resume
+{
+    public string _name;
+    public List<Job> _job = new List<Job>();
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        foreach (Job j in _job)
+        {
+            j.Display();
+        }
+    }
+}
